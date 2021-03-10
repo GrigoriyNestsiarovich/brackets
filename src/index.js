3be9c44e3b2a9f22c1c205e6da1e9ac6bcd51a68
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-03-10 23:56:18 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const lBracket = [];
+    const rBracket = [];
+    const array = [];
+    str = str.split('');
+    
+    bracketsConfig.forEach(item => {
+        lBracket.push(item[0]);
+        rBracket.push(item[1]);
+    });
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < lBracket.length; j++) {
+            if (lBracket[j] == rBracket[j] && str[i] == rBracket[j]) {
+                if (str[i] == rBracket[array[array.length - 1]]) {
+                    array.pop(j);
+                } else {
+                    array.push(j);
+                }
+            } 
+            else if (str[i] == lBracket[j]) {
+                array.push(j);
+            }
+             else if (str[i] == rBracket[j]) {
+                if (j == array[array.length - 1]) {
+                    array.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return array.length == 0;
 }
